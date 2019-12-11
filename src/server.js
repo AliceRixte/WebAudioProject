74de18cf51a83c78375c6b7b1089383378637a90
Alice Rixte <alice.rixte@gmail.com>
2019-12-11 15:32:45 +0100

    main first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+var http = require("http"),
+    fs = require("fs");
+
+function start() {
+    fs.readFile('./src/index.html', function (err, html) {
+        if (err) {
+            throw err;
+        }
+        http.createServer(function (request, response) {
+            response.writeHeader(200, { "Content-Type": "text/html" });
+            response.write(html);
+            response.end();
+        }).listen(1337);
+    });
+}
+
+exports.start = start;
