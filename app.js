a880ed2156f7d7f39a31438731123744e4f533ad
Alice Rixte <alice.rixte@gmail.com>
2019-12-12 11:05:57 +0100

    Working express server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+var root = '/';
+var public_path = path.join(__dirname, 'public');
+
+app.use(express.static(public_path));
+app.get(root, (req, res) => {
+    res.sendFile(path.join(public_path,'index.html'));
+});
+
+app.get('*', (req, res) => {
+    console.log("404 : don't know this request ");
+});
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+
