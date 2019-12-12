9914451163ba8e74efb5c49664cbd1f26c4f2b04
Alice Rixte <alice.rixte@gmail.com>
2019-12-12 16:10:47 +0100

    Some JSON stuff

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const manageFiles = require('./src/manageFiles.js');
+console.log(manageFiles.getAllFileNames('./public'));
+
 var root = '/';
 var public_path = path.join(__dirname, 'public');
 
