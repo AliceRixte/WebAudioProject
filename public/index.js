878e913357dc9c01a1c75142ee47e550f53ab8fd
Alice Rixte <alice.rixte@gmail.com>
2019-12-22 11:24:38 +0100

    create dropdown list for sample sets

--- public/index.js
+++ public/index.js
@@ -1,19 +1,29 @@
 //import * as manageFiles from '/js/manageFiles.js';
-
+import * as loadFiles from "/js/loadFiles.js"
 const audioContext = new AudioContext(); //crée context audio
 const $btn = document.querySelector('button'); //donner elt button avec semble de functionnalité
 let resumed = false;
 
-/*console.log("Index loaded");
-for (let file of manageFiles.getAllFileNames('/resources/Test1')){
-    console.log(file);
-};*/
+//load json file from server
+var file_tree;
+fetch('/fileTree.json')
+    .then(response => response.json())
+    .then(jsonResponse => file_tree = jsonResponse);
+
+var set_list = document.createElement("select");
 
 const callback = async function () {
     if (!resumed) {
         await audioContext.resume();
         resumed = true;
     }
+    Object.keys(file_tree["sample_sets"]).forEach((key) => {
+        var new_option = document.createElement("option");
+        new_option.text = key;
+        set_list.add(new_option);
+    })
+    document.querySelector("body").append(set_list);
+    
     const now = audioContext.currentTime;
 
     const env = audioContext.createGain();
