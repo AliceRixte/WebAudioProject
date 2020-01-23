97b8758c403d5047baba1176a46d7da0125c0543
Alice Rixte <alice.rixte@disroot.org>
2020-01-23 11:49:46 +0100

    c'est de plus en plus jouli

--- public/js/loadFiles.js
+++ public/js/loadFiles.js
@@ -55,7 +55,6 @@ export async function loadSampleSet(sample_set_tree, path_to_sample_set, audio_c
     Object.keys(sample_set_tree).forEach(category =>  {
         if (i < 4) {
             sample_set["category_names"].push(category);
-            console.log(category);
             if (sample_set_tree[category]) {
                 sample_set_tree[category].forEach(async sample => {
                     sample_set["sample_names"][i].push(sample);
